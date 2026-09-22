486215347519818161984d242101a6ef889225a1
favierjules-rgb <favierjules.contact@gmail.com>
2026-09-22 19:34:11 +0200

    chore(nolio): remove integration code

--- components/student/ProfilPageContent.tsx
+++ components/student/ProfilPageContent.tsx
@@ -16,7 +16,6 @@ import { StudentOnboardingDetailModal } from "@/components/student/StudentOnboar
 import { SubscriptionSection } from "@/components/student/SubscriptionSection";
 import { WeightEvolutionCard } from "@/components/student/WeightEvolutionCard";
 import { SectionIndisponible } from "@/components/pwa/SectionIndisponible";
-import { NolioConnexionCard } from "@/components/student/NolioConnexionCard";
 import { NotificationsSection } from "@/components/student/NotificationsSection";
 import { useEtatOfflineEleve } from "@/hooks/useEtatOfflineEleve";
 import { useStudentProfile, type StudentProfileState } from "@/hooks/useStudentProfile";
@@ -145,10 +144,6 @@ export function ProfilPageContent({
 
         <div className="mb-6">
           <ChangePasswordSection />
-        </div>
-
-        <div className="mb-6">
-          <NolioConnexionCard />
         </div>
 
         <NewsletterPreferenceToggle />
